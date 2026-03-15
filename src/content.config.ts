4f89963da498aefa286d732ea8c61d851c8a383e
Felix Schneider <99918022+trueberryless@users.noreply.github.com>
2026-03-15 15:44:12 +0100

    wip

--- src/content.config.ts
+++ src/content.config.ts
@@ -86,23 +86,8 @@ const docsComponentsCollection = defineCollection({
   schema: docsComponentSchema,
 });
 
-const blogPostSchema = z.object({
-  title: z.string(),
-  description: z.string(),
-  date: z.coerce.date(),
-  author: z.string().default("Anonymous"),
-  image: z.string().optional(),
-  tags: z.array(z.string()).default([]),
-});
-
-const blogCollection = defineCollection({
-  loader: glob({ pattern: "**/*.mdx", base: "./src/content/blog" }),
-  schema: blogPostSchema,
-});
-
 export const collections = {
   pages: pagesCollection,
   "docs-pages": docsPagesCollection,
   "docs-components": docsComponentsCollection,
-  blog: blogCollection,
 };
